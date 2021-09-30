5591c6f6c334af0924a1a706f14d6cf2cc8f3dc6
aralce <ariel.cerfoglia@gmail.com>
2021-09-29 21:57:08 -0300

    Sensor manual test added

---
+++ Implementation/manual_test/sensor_manual_test.cpp
@@ -0,0 +1,67 @@
+#include "sensor_manual_test.h"
+#include "cmsis_os.h"
+#include "../../classes/sensor.h"
+#include "cmsis_os.h"
+#include "stm32f4xx_hal.h"
+
+static sensor *sensor[2];
+extern UART_HandleTypeDef huart1;
+
+void sensor_switch_test_init()
+{
+	/*GPIO Initialization*/
+	GPIO_InitTypeDef GPIO_init_struct = {0};
+	GPIO_init_struct.Pin = GPIO_PIN_2;
+	GPIO_init_struct.Mode = GPIO_MODE_INPUT;
+	GPIO_init_struct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOG, &GPIO_init_struct);
+	GPIO_init_struct.Pin = GPIO_PIN_3;
+	HAL_GPIO_Init(GPIOG, &GPIO_init_struct);
+
+	/*Initialization of sensor*/
+	sensor[0] = new sensor_switch(GPIOG, GPIO_PIN_2, true);
+	sensor[1] = new sensor_switch(GPIOG, GPIO_PIN_3, false);
+}
+
+void sensor_switch_test_run()
+{
+	/*Read sensors and show if a sensor is active on LED3 and LED4*/
+	bool isHigh = sensor[0]->read();
+	HAL_GPIO_WritePin(GPIOG, GPIO_PIN_13, isHigh ? GPIO_PIN_SET : GPIO_PIN_RESET ); //LED3-SENSOR0(PG2)
+	isHigh = sensor[1]->read();
+	HAL_GPIO_WritePin(GPIOG, GPIO_PIN_14, isHigh ? GPIO_PIN_SET : GPIO_PIN_RESET ); //LED4-SENSOR1(PG3)
+
+	{
+	uint8_t string[] = "\nThe number of sensors is: ";
+	HAL_UART_Transmit(&huart1, string, sizeof(string)/sizeof(string[0]), 500/*ms*/ );
+	uint8_t sensor_num[2] = {(uint8_t)sensor_switch::_instances_number, (uint8_t)'\n'};
+	HAL_UART_Transmit(&huart1, sensor_num, sizeof(sensor_num)/sizeof(sensor_num[0]), 500/*ms*/ );
+	}
+
+	/*Delete 1 sensor and show the number of sensors on UART*/
+	osDelay(500);
+	if(sensor_switch::_instances_number)
+		delete sensor[0];
+	{
+	uint8_t string[] = "\nThe number of sensors is: ";
+	HAL_UART_Transmit(&huart1, string, sizeof(string)/sizeof(string[0]), 500/*ms*/ );
+	uint8_t sensor_num[2] = {(uint8_t)sensor_switch::_instances_number , (uint8_t)'\n'};
+	HAL_UART_Transmit(&huart1, sensor_num, sizeof(sensor_num)/sizeof(sensor_num[0]), 500/*ms*/ );
+	}
+
+	osDelay(500);
+	/*Delete 1 sensor and show the number of sensors on UART*/
+	if(sensor_switch::_instances_number)
+		delete sensor[1];
+	{
+	uint8_t string[] = "\nThe number of sensors is: ";
+	HAL_UART_Transmit(&huart1, string, sizeof(string)/sizeof(string[0]), 500/*ms*/ );
+	uint8_t sensor_num[2] = {(uint8_t)sensor_switch::_instances_number , (uint8_t)'\n'};
+	HAL_UART_Transmit(&huart1, sensor_num, sizeof(sensor_num)/sizeof(sensor_num[0]), 500/*ms*/ );
+	}
+
+	/*Initialization of sensor*/
+	sensor[0] = new sensor_switch(GPIOG, GPIO_PIN_2, true);
+	sensor[1] = new sensor_switch(GPIOG, GPIO_PIN_3, false);
+	osDelay(500);
+}
---
+++ Implementation/manual_test/sensor_manual_test.h
@@ -0,0 +1,37 @@
+/**Sensor_manual_test.h
+ * This file contain the manual tests for the sensor class.
+ *
+ * FAQ:
+ * How do I configure project to run test?
+ * 1) You must create a project in stm32CubeIde with MXCube. Select the STM32F429 discovery evaluation kit.
+ * 2) You may use the default configuration for this board.
+ * 3) Change the name of "main.c" to "main.cpp"
+ * 4) Copy manual_test folder and classes folder to the project.
+ * 5) You should check that manual_test folder and classes folder is not excluded from build.
+ * 5) Copy cmain.cpp to core/Src and cmain.h to core/Inc.
+ * 6) Copy the function test to run in cmain() in cmain.cpp and follow the steps described on the selected test.
+ */
+#ifndef SENSOR_MANUAL_TEST_H
+#define SENSOR_MANUAL_TEST_H
+
+
+/*Sensor Switch Class:
+ * 1- Create a sensor attached to a GPIO as normal_open or normal_close.
+ * 2- Read a value from GPIO.
+ * 3- Create a sensor increase the _instances_number in sensor.
+ * 4- Delete a sensor decrease the _instances_number in sensor.
+ * 5- If the sensor is normal_open when the sensor is close is read as High.
+ * 6- If the sensor is normal_close when the sensor is open is read as High.
+ */
+
+/**Sensor Switch Class test using class sensor pointer
+ * 1) Call function sensor_switch_test_init in init_cmain() in cmain.cpp
+ * 2) Call function sensor_switch_test_run in cmain() in cmain.cpp
+ * 3) Check the PA9 port and GND with a logic analyzer to see UART at 115200. The sensor instances are shown there.
+ * 4) Check that connecting PG2 port to GND turn on the green led. PG2 is configured as a normal_close sensor and the led indicate when sensor is active.
+ * 5) Check that connecting PG3 port to GND turn off the red led. PG3 is configured as a normal_open sensor and the led indicate when sensor is active.
+ */
+void sensor_switch_test_init();
+void sensor_switch_test_run();
+
+#endif //SENSOR_MANUAL_TEST_H
