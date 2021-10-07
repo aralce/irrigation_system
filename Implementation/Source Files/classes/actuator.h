3a0f53daaeaf1eda21c32755a04e36fc815d606a
aralce <ariel.cerfoglia@gmail.com>
2021-10-07 15:13:50 -0300

    Change on Actuator class

--- Implementation/Source Files/classes/actuator.h
+++ Implementation/Source Files/classes/actuator.h
@@ -6,51 +6,51 @@
 #ifndef ACTUATOR_H
 #define ACTUATOR_H
 
-#include "sensor.h"
 #include <type_traits>
 #include <iostream>
+#include "Sensor.h"
 
 extern "C"{
 	#include "stm32f4xx_hal.h"
 }
 
 
-class actuator
+class Actuator
 {
 public:
 	virtual void set( bool set_ON ) = 0;
 	virtual bool is_ON() = 0;
 	virtual bool has_sensor() = 0;
 	virtual uint32_t read_sensor() = 0;
-	virtual ~actuator() = default;
+	virtual ~Actuator() = default;
 };
 
 
-class actuator_ON_OFF : actuator
+class Actuator_ON_OFF : public Actuator
 {
 public:
 	/*Constructors*/
-	explicit actuator_ON_OFF(GPIO_TypeDef *gpio, uint16_t pin ) :
+	explicit Actuator_ON_OFF(GPIO_TypeDef *gpio, uint16_t pin ) :
 			_gpio(gpio), _pin(pin), _has_sensor(false), _sensor_ptr(nullptr) { set(false); }
-	explicit actuator_ON_OFF(GPIO_TypeDef *gpio, uint16_t pin, sensor *sensor_ptr) :
+	explicit Actuator_ON_OFF(GPIO_TypeDef *gpio, uint16_t pin, Sensor *sensor_ptr) :
 			_gpio(gpio), _pin(pin), _has_sensor(true), _sensor_ptr(sensor_ptr) { set(false); }
-	actuator_ON_OFF() = delete;
+	Actuator_ON_OFF() = delete;
 
 	/*Main functions*/
-	void set( bool set_ON );
-	bool is_ON(){ return _is_ON; }
-	bool has_sensor(){ return _has_sensor; }
-	uint32_t read_sensor(){ return _has_sensor ? _sensor_ptr->read() : 0; }
+	void set( bool set_ON ) override;
+	bool is_ON() override{ return _is_ON; }
+	bool has_sensor() override{ return _has_sensor; }
+	uint32_t read_sensor() override{ return _has_sensor ? _sensor_ptr->read() : 0; }
 
 	/*Secondary functions*/
-	bool set_sensor(sensor *sensor_ptr){ _sensor_ptr = sensor_ptr; return _has_sensor = true; }
-	bool remove_sensor(){ if(_has_sensor){ _sensor_ptr = nullptr; return !(_has_sensor = false); } };
+	bool set_sensor(Sensor *sensor_ptr){ _sensor_ptr = sensor_ptr; return _has_sensor = true; }
+	bool remove_sensor();
 
 private:
   	GPIO_TypeDef* _gpio = nullptr;
     uint16_t _pin;
     bool _has_sensor;
-    sensor *_sensor_ptr = nullptr;
+    Sensor *_sensor_ptr = nullptr;
     bool _is_ON = false;
 };
 
