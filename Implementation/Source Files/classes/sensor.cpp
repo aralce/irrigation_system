6c6aa0fe6cc865f58090b1cd3cef5d3aa04acbf7
aralce <ariel.cerfoglia@gmail.com>
2021-10-07 15:13:27 -0300

    Change on Sensor class

--- Implementation/Source Files/classes/sensor.cpp
+++ Implementation/Source Files/classes/sensor.cpp
@@ -3,11 +3,14 @@
  * Sensor is an interface to control any type of sensor.
  * Each sensor has its own class.
 */
-#include "sensor.h"
+#include "Sensor.h"
+
 #include <limits>
 
-uint32_t sensor_switch::read()
-{
-	return HAL_GPIO_ReadPin(_gpio, _pin) ? std::numeric_limits<uint32_t>::max() : 0;
+int Sensor_switch::_instances_number = 0;
 
+uint32_t Sensor_switch::read()
+{
+	bool raw_read = _is_normal_open ? !HAL_GPIO_ReadPin(_gpio, _pin) : HAL_GPIO_ReadPin(_gpio, _pin);
+	return raw_read ? std::numeric_limits<uint32_t>::max() : 0;
 }
--- Implementation/Source Files/classes/sensor.h
+++ Implementation/Source Files/classes/sensor.h
@@ -12,27 +12,28 @@ extern "C"{
 }
 
 
-class sensor
+class Sensor
 {
 public:
 	virtual uint32_t read() = 0;
-	virtual int get_instances_num() = 0;
-	virtual ~sensor() = default;
+	virtual ~Sensor() = default;
 };
 
 
-class sensor_switch : public sensor
+class Sensor_switch : public Sensor
 {
 public:
-  explicit sensor_switch(GPIO_TypeDef *gpio, uint16_t pin) : _gpio(gpio), _pin(pin) { ++_instances_number; }
-  sensor_switch() = delete;
-  int get_instances_num(){ return _instances_number; }
-  uint32_t read();
+  explicit Sensor_switch(GPIO_TypeDef *gpio, uint16_t pin, bool is_normal_open) :
+  	  	  _gpio(gpio), _pin(pin), _is_normal_open(is_normal_open) { ++_instances_number; }
+  Sensor_switch() = delete;
+  ~Sensor_switch(){ --_instances_number; }
+  uint32_t read() override;
+  static int _instances_number;
 
 private:
   	GPIO_TypeDef* _gpio = nullptr;
     uint16_t _pin = 0;
-    static  int _instances_number = 0;
+    bool _is_normal_open;
 };
 
 
