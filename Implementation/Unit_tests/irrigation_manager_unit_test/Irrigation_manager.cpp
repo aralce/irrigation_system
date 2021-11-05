12dd5d3937c1dcec90631c9b39d1e88b77591b87
aralce <ariel.cerfoglia@gmail.com>
2021-11-05 09:35:42 -0300

    Irrigation Manager: Implementation to pass tests

--- Implementation/Unit_tests/irrigation_manager_unit_test/Irrigation_manager.cpp
+++ Implementation/Unit_tests/irrigation_manager_unit_test/Irrigation_manager.cpp
@@ -1,12 +1,14 @@
 #include "Irrigation_manager.h"
 
-Irrigation_manager::Irrigation_manager(Irrigation_zone* zones[]){
-    for(auto i=0; i<MAX_ZONES; ++i)
+Irrigation_manager::Irrigation_manager(Irrigation_zone* zones[], Calendar_routine* calendar[]){
+    for(auto i=0; i<MAX_ZONES; ++i){
         _zones[i] = zones[i];
+        _calendar[i] = calendar[i];
+    }
 }
 
 bool Irrigation_manager::irrigate(uint8_t zone_from_0, bool must_irrigate){
-    if(zone_from_0 > MAX_ZONES)
+    if(zone_from_0 >= MAX_ZONES)
         return false;
     _zones[zone_from_0]->irrigate(must_irrigate);
     return true;
@@ -23,4 +25,10 @@ bool Irrigation_manager::is_healthy(uint8_t* zone_with_error = nullptr){
         }
     }
     return are_zones_ok;
+}
+
+bool Irrigation_manager::add_event(uint8_t zone_from_0, const tm start_time, const uint32_t duration_in_minutes){
+    if(zone_from_0 >= MAX_ZONES)
+        return false;
+    return _calendar[zone_from_0]->add_event(start_time, duration_in_minutes);
 }
--- Implementation/Unit_tests/irrigation_manager_unit_test/Irrigation_manager.h
+++ Implementation/Unit_tests/irrigation_manager_unit_test/Irrigation_manager.h
@@ -3,8 +3,10 @@
 */
 #ifndef IRRIGATION_MANAGER_H
 #define IRRIGATION_MANAGER_H
+#include <ctime>
 #include "Irrigation_manager_config.h"
 #include "mock-irrigation_zone.h"
+#include "mock-calendar_routine.h"
 
 class Irrigation_manager{
 public:
@@ -12,12 +14,14 @@ public:
     Irrigation_manager() = delete;
     Irrigation_manager(Irrigation_manager& ) = delete;
     Irrigation_manager& operator=(Irrigation_manager&) = delete;
-    Irrigation_manager(Irrigation_zone* zone[]);
+    Irrigation_manager(Irrigation_zone* zone[], Calendar_routine* calendar[]);
     //Member functions
     bool irrigate(uint8_t zone_from_0, bool must_irrigate);
     bool is_healthy(uint8_t* zone_with_error);
+    bool add_event(uint8_t zone_from_0, const tm start_time, const uint32_t duration_in_minutes);
 private:
     Irrigation_zone* _zones[MAX_ZONES];
+    Calendar_routine* _calendar[MAX_ZONES];
 };
 
 #endif
